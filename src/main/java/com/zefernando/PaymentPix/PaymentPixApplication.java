14b18f21f63f721fe8f166fbc6def7fcf25ec7ab
ze-fernando <jfenginner935@gmail.com>
2024-01-23 21:18:58 -0300

    init project

---
+++ src/main/java/com/zefernando/PaymentPix/PaymentPixApplication.java
@@ -0,0 +1,13 @@
+package com.zefernando.PaymentPix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zefernando/PaymentPix/PaymentPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zefernando.PaymentPix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
