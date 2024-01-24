975e1ca7903690b78725ec97170bf80192e547ea
ze-fernando <jfenginner935@gmail.com>
2024-01-23 21:43:14 -0300

    create: class user

---
+++ src/main/java/com/zefernando/PaymentPix/entity/User.java
@@ -0,0 +1,40 @@
+package com.zefernando.PaymentPix.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String verificationToken;
+
+    private Boolean isActive;
+
+    public User(Long id, String name, String email, String password, String verificationToken, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.verificationToken = verificationToken;
+        this.isActive = isActive;
+    }
+
+    public User() {
+    }
+}
