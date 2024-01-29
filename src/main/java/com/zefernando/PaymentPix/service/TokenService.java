b07d28601339a1030a2a77d551652de1a0cdc6ea
ze-fernando <jfenginner935@gmail.com>
2024-01-29 20:13:46 -0300

    feat: create class token service

--- src/main/java/com/zefernando/PaymentPix/service/TokenService.java
+++ src/main/java/com/zefernando/PaymentPix/service/TokenService.java
@@ -3,6 +3,7 @@ package com.zefernando.PaymentPix.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.zefernando.PaymentPix.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,25 +14,37 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-
     @Value("${jwt.secret}")
     private String secret;
 
     public String generateToken(User user){
         try{
-            Algorithm alg = Algorithm.HMAC256(secret);
-            String token = JWT.create().withIssuer("auth")
-                    .withSubject(user.getEmail()).withExpiresAt(Expiration())
-                    .sign(alg);
-
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
             return token;
-        } catch (JWTCreationException exception){
-            throw new RuntimeException("The token was not generated", exception);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token", exception);
         }
     }
 
-    private Instant Expiration(){
-        return LocalDateTime.now().plusMinutes(1)
-                .toInstant(ZoneOffset.of("-03:00"));
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token is not valid", exception);
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
